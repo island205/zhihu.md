37835f6ba60d50a5bd18626c85696677ac0e01c7
island205 <island205@gmail.com>
2019-03-02 21:49:39 +0800

    feat: add title and title image to md
    format code

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const puppeteer = require('puppeteer');
-const h2m = require('h2m');
+const puppeteer = require("puppeteer");
+const h2m = require("h2m");
 
-const zhuanlanURL = 'https://zhuanlan.zhihu.com/p/55357377'
+const zhuanlanURL = "https://zhuanlan.zhihu.com/p/57842018";
 
-function wait (ms) {
+function wait(ms) {
   return new Promise(resolve => setTimeout(() => resolve(), ms));
 }
 
 function replaceAll(target, search, replacement) {
   return target.split(search).join(replacement);
-};
+}
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(zhuanlanURL, {
-    waitUntil: 'load'
+    waitUntil: "load"
   });
 
   // 滚动页面到底，加载预加载图片
   // https://www.screenshotbin.com/blog/handling-lazy-loaded-webpages-puppeteer
   // Get the height of the rendered page
-  const bodyHandle = await page.$('body');
-  const {
-    height
-  } = await bodyHandle.boundingBox();
+  const bodyHandle = await page.$("body");
+  const { height } = await bodyHandle.boundingBox();
   await bodyHandle.dispose();
 
   // Scroll one viewport at a time, pausing to let content load
@@ -38,21 +36,43 @@ function replaceAll(target, search, replacement) {
     viewportIncr = viewportIncr + viewportHeight;
   }
 
-
-  const articleHTML = await page.evaluate(() => {
+  const article = await page.evaluate(() => {
     // 修复代码格式
-    document.querySelectorAll('pre').forEach(pre => pre.innerHTML = pre.innerText);
+    document
+      .querySelectorAll("pre")
+      .forEach(pre => (pre.innerHTML = pre.innerText));
     // 避免重复
-    document.querySelectorAll('noscript').forEach(el => el.remove());
-    document.querySelectorAll('figcaption').forEach(el => el.remove());
-    return document.querySelector('.Post-RichText').innerHTML;
+    document.querySelectorAll("noscript").forEach(el => el.remove());
+    document.querySelectorAll("figcaption").forEach(el => el.remove());
+    let titleImage = document.querySelector(".TitleImage");
+    titleImage = titleImage ? titleImage.src : null;
+    return {
+      articleHTML: document.querySelector(".Post-RichText").innerHTML,
+      titleImage,
+      title: document.querySelector(".Post-Title").innerText
+    };
   });
 
-  let articleMD = h2m(articleHTML);
+  let articleMD = h2m(article.articleHTML);
+
+  articleMD = `# ${article.title}
+
+![](${article.titleImage})
+
+${articleMD}
+`;
 
   // 修复链接
-  articleMD = replaceAll(articleMD, 'http://link.zhihu.com/?target=https%3A', 'https:')
-  articleMD = replaceAll(articleMD, 'http://link.zhihu.com/?target=http%3A', 'http:')
+  articleMD = replaceAll(
+    articleMD,
+    "http://link.zhihu.com/?target=https%3A",
+    "https:"
+  );
+  articleMD = replaceAll(
+    articleMD,
+    "http://link.zhihu.com/?target=http%3A",
+    "http:"
+  );
 
   console.log(articleMD);
 
